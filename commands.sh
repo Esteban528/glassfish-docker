095b4b6dbc7271499bdd7a15fa6c394440affc85
Esteban Gonzalez Florez <estebangonza2005@gmail.com>
2024-08-06 17:42:48 -0500

    fix: 🐛 Autodeploy works

--- commands.sh
+++ commands.sh
@@ -5,8 +5,5 @@ export PATH=$PATH:$GLASSFISH_HOME/bin
 
 asadmin start-domain
 # asadmin --user admin enable-secure-admin
-asadmin deploy /var/my-jakarta-app/target/mavenproject1-1.0-SNAPSHOT.war
-
-echo "ServerDeployed in http://localhost:8080/mavenproject1-1.0-SNAPSHOT"
 
 tail -f /dev/null
