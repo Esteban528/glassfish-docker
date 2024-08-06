d4e22329eeecf4a535b18e7dd697091915f90685
Esteban Gonzalez Florez <estebangonza2005@gmail.com>
2024-08-06 15:51:01 -0500

    fix: 🐛 hello world servlet

--- src/main/java/com/example/HelloWorldServlet.java
+++ src/main/java/com/example/HelloWorldServlet.java
@@ -8,7 +8,7 @@ import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
-@WebServlet("HelloWorld")
+@WebServlet("/helloworld")
 public class HelloWorldServlet extends HttpServlet {
 
   @Override
